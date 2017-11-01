dbf7e51500920d5d046b588ee9223fd31e4cef5c
Rick <654834341@qq.com>
2017-11-01 18:25:21 +0800

    Build blog page

--- mysite/test/admin.py
+++ mysite/test/admin.py
@@ -1,5 +1,14 @@
 from django.contrib import admin
-from .models import Question
-
-admin.site.register(Question)
+from .models import Question,Choice
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [("Date Info",{'fields':["pub_date"],'classes':['collapse']}),
+                (None,{'fields':["question_text"]}),]
+    inlines = [ChoiceInline]
+    search_fields = ['question_text']
+    list_display = ('question_text','pub_date','was_published_recently')
+admin.site.register(Question,QuestionAdmin)
+admin.site.register(Choice)
 # Register your models here.
